a2eff8406c0ac28c43b9266e32df7310d4783ced
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-06-17 02:02:57 +0000

    added login functionality

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -16,6 +16,29 @@ const getState = ({ getStore, setStore }) => {
 			todo: []
 		},
 		actions: {
+			login: (username, email) => {
+				const store = getStore();
+				fetch("https://3000-d8e924cf-c73b-489c-9c3f-30eb2e9739e2.ws-us0.gitpod.io/login", {
+					method: "POST",
+					headers: { "Content-type": "application-json" },
+					body: JSON.stringify({
+						username: username,
+						email: email
+					})
+				})
+					.then(res => {
+						return res.json();
+					})
+					.then(response => {
+						//console.log(loggedInUser.user);
+						localStorage.setItem("token", response.access_token);
+						setStore({ token: response.access_token });
+						console.log(localStorage.setItem);
+						return true;
+					})
+
+					.catch(error => console.error("Error:", error));
+			},
 			addTodo: item => {
 				const store = getStore();
 				fetch("https://3000-d8e924cf-c73b-489c-9c3f-30eb2e9739e2.ws-us0.gitpod.io/todo", {
